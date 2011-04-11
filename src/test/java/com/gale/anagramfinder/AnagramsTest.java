aa94abf26b1fd6a2ef7fb15585d30871af58b2f8
achesny <achesny@c2b1cd26-5903-11df-92e7-4b0514d33ac5>
2011-04-11 02:21:59 +0000

    Anagram bad solution
    git-svn-id: svn://tg-dxcvs1.oh.gale.com/GADA/craft-day/anagram-finder@8391 c2b1cd26-5903-11df-92e7-4b0514d33ac5

---
+++ src/test/java/com/gale/anagramfinder/AnagramsTest.java
@@ -0,0 +1,59 @@
+package com.gale.anagramfinder;
+
+import static com.gale.anagramfinder.Anagrams.isAnagram;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class AnagramsTest {
+	
+	@Test
+	public void isAnagram_returnsFalse() throws Exception {
+		assertThat(isAnagram("a", "b"), is(false));
+	}
+	
+	@Test
+	public void isAnagram_returnsTrue() throws Exception {
+		assertThat(isAnagram("god", "dog"), is(true));
+	}
+	
+	@Test
+	public void isAnagram_handlesDoubleLettersCorrectly() throws Exception {
+		assertThat(isAnagram("little", "tiddle"), is(false));
+	}
+	
+	@Test
+	public void isAnagram_handlesLengthesCorrectly() throws Exception {
+		assertThat(isAnagram("paddle", "daple"), is(false));
+	}
+	
+	@Test
+	public void isAnagram_handlesSimilarTerms() throws Exception {
+		assertThat(isAnagram("amortizements", "isomerization"), is(false));
+	}
+	
+	@Test
+	public void findAnagrams_returnsSingleAnagram() throws Exception {
+		List<String> anagram = (List<String>)Arrays.asList("dog", "god");
+		assertThat(Anagrams.find("dog", "god"), is((List<List<String>>)Arrays.asList(anagram)));
+	}
+	
+	@Test
+	public void findAnagrams_doesNotContainListsOfSingleTerms() throws Exception {
+		List<String> anagram = (List<String>)Arrays.asList("dog", "god");
+		assertThat(Anagrams.find("dog", "god", "anything"), is((List<List<String>>)Arrays.asList(anagram)));
+	}
+	
+	@Ignore
+	@Test
+	public void findAnagrams_doesNotReturnListsOfWordsThatAreNotAnagrams() throws Exception {
+		List<List<String>> anagramList = new ArrayList<List<String>>();
+		assertThat(Anagrams.find("amortizements", "isomerization", "memorizations"), is(anagramList));
+	}
+}
